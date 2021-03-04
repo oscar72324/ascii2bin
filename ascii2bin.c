626a20132be0d9992099f03ea91fb1268d7684ab
Oscar Ibarra <oi893101@ssh.sandbox.csun.edu>
2021-03-04 12:04:16 -0800

    Initial version

--- ascii2bin.c
+++ ascii2bin.c
@@ -3,21 +3,45 @@
 
 int main()
 {
-    int offset = 48;  // ascii value of '0'
+    int offset = 48;
     int number = 0;
     char ascii_value;
 
     int retval = read(0, &ascii_value, 1);
     while ((retval == 1) && (ascii_value != '\n')) {
         int digit = ascii_value - offset;
-        if ((digit == 1) || (digit == 0))
+        if ((digit == 1) || (digit == 0)){
             number = (number << 1) + digit;
         retval = read(0, &ascii_value, 1);
+    }
+    else {
+    return 1;
+    }
     }
     printf("%u\n", number);
 
     return 0;
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
